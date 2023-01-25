17a028156e362842d50c8f6f24f4b8dd42ea7c1e
ChamRun <chrm@aut.ac.ir>
2023-01-25 06:13:39 +0330

    add `test_a_b` function

--- regression_calculator.py
+++ regression_calculator.py
@@ -27,6 +27,13 @@ def main():
     #
     # # We want to find the a and b values for the regression line.
     a, b = find_a_b_based_on_least_squares(train_df)
+    test_result = test_a_b(a, b, test_df)
+
+    for i in range(len(test_result)):
+        print(f'Read value: {test_result[i][0]}'
+              f'\nEstimated value: {test_result[i][1]}'
+              f'\nError: {test_result[i][2]}\n')
+
 
 def multiply_dataframes(df1, df2):
     # convert dataframes to numpy matrices
@@ -49,5 +56,20 @@ def find_a_b_based_on_least_squares(df):
     a = (n * sum_xy - sum_x * sum_y) / (n * sum_x_squared - sum_x ** 2)
     b = (sum_y - a * sum_x) / n
     return a, b
+
+
+def test_a_b(a, b, df):
+    x = df['x'].values
+    y = df['y'].values
+    test_result = []
+    for i in range(len(x)):
+        read_value = y[i]
+        estimated_value = a * x[i] + b
+        error = abs(read_value - estimated_value)
+        test_result.append([read_value, estimated_value, error])
+
+    return test_result
+
+
 if __name__ == '__main__':
     main()
